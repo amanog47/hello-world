f79b7dc841dcc96100e3600d99b177157b6d763d
Aman Pandey <amanog47@gmail.com>
2016-06-15 17:45:03 +0530

    new file
    random

---
+++ justanother.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define MP make_pair
+#define pb push_back
+#define rep(i,n) for(int i=0;i<n;i++)
+#define REP(i,a,b) for(int i=a;i<=b;i++)
+#define PER(i,a,b) for(int i=b;i>=a;i--)
+#define X first
+#define Y second
+ 
+//i/o
+#define inp(n) scanf("%d",&n)
+#define inpl(n) scanf("%lld",&n)
+#define inp2(n,m) inp(n), inp(m)
+#define inp2l(n,m) inpl(n), inpl(m)
+ 
+ 
+//cost
+#define lli long long int
+#define MOD 1000000007
+#define MOD_INV 1000000006
+#define MAX 100009
+#define INF 999999999
+#define mp make_pair
+
+//iterators
+#define vitr std::vector<int>::iterator
+int main()
+{
+	
+	return 0;
+}
